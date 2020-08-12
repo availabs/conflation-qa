52137d26eb0414db054059c191fac9df681272ea
alex <am3081@gmail.com>
2020-08-12 13:31:08 -0400

    api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import AvlMap from "./AvlMap"
+// import TransitLayerFactory from './layers/TransitLayer'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ height: "100vh" }}>
+        <AvlMap
+          dragPan={true}
+          style={ 'mapbox://styles/am3081/cjqqukuqs29222sqwaabcjy29' }
+          header="Transit Data"/>
+      </div>
     </div>
   );
 }
