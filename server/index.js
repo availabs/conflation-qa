f8a4eb56e3a75c8f5af91915a1d2e3c4a0498692
Paul Tomchik <pauljtomchik@gmail.com>
2020-08-13 10:58:08 -0400

    CORS for server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 #!/usr/bin/env node
 
 const restify = require("restify");
+const corsMiddleware = require('restify-cors-middleware')
 
 const {getGtfsEdges} = require('./src/daos/GtfsNetworkDao')
 
 const PORT = process.env.PORT || 8080
 
 const server = restify.createServer();
+
+// https://www.npmjs.com/package/restify-cors-middleware#usage
+const cors = corsMiddleware({
+  origins: ['*'],
+})
+
+server.pre(cors.preflight)
+server.use(cors.actual)
 
 server.get("/gtfs-edges", (_req, res, next) => {
   const gtfsEdges = getGtfsEdges()
